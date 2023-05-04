1027eac14809199bd8542db20f307a8b2f6cbc96
pvsantos4 <paulovictor494@gmail.com>
2023-05-04 11:49:02 -0300

    add interpolation to Angular templates in order to display dynamic data in a template.

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -7,10 +7,24 @@ import { HousingLocation } from '../housinglocation';
   standalone: true,
   imports: [CommonModule],
   template: `
-  <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
-  `,
-  styleUrls: ['./housing-location.component.css']
+    <section class="listing">
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+    </section>
+    `,
+  styleUrls: ['./housing-location.component.css'],
 })
+
 export class HousingLocationComponent {
+
   @Input() housingLocation!: HousingLocation;
+
 }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
